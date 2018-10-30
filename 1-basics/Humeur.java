27d4163aa801322422df07d462389c94bcb27033
adjasam1 <samuel.sabot.lp2@gmail.com>
2018-10-30 09:28:10 +0100

    apprivoiser éclipse

--- 1-basics/Humeur.java
+++ 1-basics/Humeur.java
@@ -1,39 +1,62 @@
 import java.util.Scanner;
 
-/****************** REMARQUES JOSSELIN *****************
-Le code est propre.
-
-Quelques tests inutiles, par ex pas nécessaire de tester
-reponse.length()<= 20 dans le else if de
-reponse.length()>= 20
-****************** REMARQUES JOSSELIN *****************/
 public class Humeur {
 
-  public static void main(String[] args) {
+	static Scanner saisie;
 
-    Scanner saisie = new Scanner (System.in);
+	public static void main(String[] args) {
 
-    System.out.println("Décrivez votre humeur :");
+		saisie = new Scanner(System.in);
 
-    String reponse = saisie.nextLine();
+		String humeur = null;
 
-    if (!Character.isUpperCase(reponse.charAt(0)))  {
-      System.out.println("Erreur, votre reponse doit commencer par une majuscule");
-    }
-    else if ( reponse.charAt(reponse.length()-1) != '.')  {
-      System.out.println("Votre reponse doit se terminee par un '.'");
-    }
-    else if ((reponse.length() <= 5)){
-      System.out.println("faites un effort, j’aimerais en savoir plus");
-    }
-    else if ((reponse.length() >= 20)){
-      System.out.println("pourriez-vous être plus concis");
-    }
-    else if (((reponse.length())%2 == 0) && (reponse.length() > 5) && (reponse.length()<= 20)) {
-      System.out.println("c’est très intéressant, on peut en parler plus tard?");
-    }
-    else if ((reponse.length())%2 == 1  && (reponse.length() > 5) && (reponse.length()<= 20)) {
-      System.out.println("c’est bien ce que je pensais");
-    }
-  }
+		reponseHumeur(saisieHumeur(humeur));
+		saisie.close();
+
+	}
+
+	// SAISIE HUMEUR JUSQU'A CE QUE TOUTES LES CONDITIONS SOIENT VALIDEES
+	public static String saisieHumeur(String humeur) {
+
+		do {
+
+			System.out.print("Décrivez votre humeur : ");
+			humeur = saisie.nextLine();
+
+			if (!Character.isUpperCase(humeur.charAt(0))) { // COMMENCE PAR UNE MAJUSCULE
+				System.out.println("Erreur, votre reponse doit commencer par une majuscule.");
+			}
+
+			else if (humeur.charAt(humeur.length() - 1) != '.') { // FINIT PAR UN POINT
+				System.out.println("Erreur, votre reponse doit se terminer par un '.'.");
+			}
+
+			else if ((humeur.length() <= 5)) { // PLUS DE 5 CARACTERES
+				System.out.println("Faites un effort, j’aimerais en savoir plus");
+			}
+
+			else if ((humeur.length() >= 20)) { // MOINS DE 20 CARACTERES
+				System.out.println("Pourriez-vous être plus concis");
+			}
+
+		} while (!Character.isUpperCase(humeur.charAt(0)) 
+				|| humeur.charAt(humeur.length() - 1) != '.'
+				|| humeur.length() <= 5 || humeur.length() >= 20);
+		return humeur;
+	}
+	
+	public static String reponseHumeur(String humeur) {
+		
+		if (humeur.length()%2 == 0) { // NOMBRE CARACTERES PAIR
+			System.out.println("C'est très intéressant ! On peut en parler plus tard ?");
+		}
+		
+		else if (humeur.length()%2 == 1) { // NOMBRE CARACTERES IMPAIR
+			System.out.println("C'est bien ce que je pensais !");
+		}
+		
+		return humeur;
+		
+	}
+
 }
